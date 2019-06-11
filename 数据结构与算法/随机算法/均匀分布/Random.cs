8c00663156769b78d74950001f5cb05bf5be9d9a
C41 <c41hahaha@163.com>
2019-06-11 11:08:07 +0800

    update

---
+++ 数据结构与算法/随机算法/均匀分布/Random.cs
@@ -0,0 +1,7 @@
+private static readonly Random random = new Random();
+
+//以均匀分布随机返回[min, max)
+private static double Range(double min, double max)
+{
+	return random.NextDouble() * (max - min) + min;
+}
