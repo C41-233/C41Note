f247f7a1f910f9db3cbf8703a6c6c1039ffca467
C41 <c41hahaha@163.com>
2019-06-25 16:49:07 +0800

    update

--- 数据结构与算法/随机算法/正态分布（高斯分布）/Random.cs
+++ 数据结构与算法/随机算法/正态分布（高斯分布）/Random.cs
@@ -6,7 +6,7 @@
 /// <returns>返回范围为(-∞, +∞)</returns>
 public static double Normal(double miu, double sigma)
 {
-	var x1 = RandomRange(0, 1);
-	var x2 = RandomRange(0, 1);
+	var x1 = RandomDouble(0, 1);
+	var x2 = RandomDouble(0, 1);
 	return sigma * Math.Sqrt(-2 * Math.Log(x1)) * Math.Cos(2 * Math.PI * x2) + miu;
 }
