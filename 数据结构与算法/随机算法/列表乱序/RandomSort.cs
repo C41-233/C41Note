c15ffef7f1063c7feb1386dc8c1b34ec2957bcf0
C41 <c41hahaha@163.com>
2019-08-30 17:17:25 +0800

    \t

--- 数据结构与算法/随机算法/列表乱序/RandomSort.cs
+++ 数据结构与算法/随机算法/列表乱序/RandomSort.cs
@@ -1,11 +1,11 @@
 //原址乱序
 public static void RandomSort<T>(List<T> list)
 {
-	for (var i = 0; i < list.Count; i++)
-	{
-		var to = RandomRange(i, list.Count); //随机选取范围[i, list.Count)
-		var tmp = list[to];
-		list[to] = list[i];
-		list[i] = tmp;
-	}
+    for (var i = 0; i < list.Count; i++)
+    {
+        var to = RandomRange(i, list.Count); //随机选取范围[i, list.Count)
+        var tmp = list[to];
+        list[to] = list[i];
+        list[i] = tmp;
+    }
 }
