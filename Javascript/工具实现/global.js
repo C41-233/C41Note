4576de0cbef7476cec18c896c866929dc673f192
C41 <c41hahaha@163.com>
2020-03-13 19:21:09 +0800

    js

---
+++ Javascript/工具实现/global.js
@@ -0,0 +1,9 @@
+(function(global){
+    
+})(typeof window !== 'undefined'
+   ? window
+   : (typeof process === 'object' &&
+      typeof require === 'function' &&
+      typeof global === 'object')
+     ? global
+     : this);
